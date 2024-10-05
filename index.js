b6eb88e99fa759a85171d6ea60b0ce5549f500e4
Neeraj Koshti <neerajkoshti@Neerajs-MacBook-Air.local>
2024-10-05 13:42:15 +0530

    test

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
-const express = require('express')
-const app = express()
-const PORT = process.env.PORT || 5000
-app.use(express.json({ extended: false }))
-app.get('/', (req, res) => {
-    res.json({ msg: 'hello' })
-})
-
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 5000;
+app.use(express.json({ extended: false }));
+app.get("/", (req, res) => {
+  res.json({ msg: "hello" });
+});
 
 app.listen(PORT, () => {
-    console.log(`server is running on port ${PORT}`)
-})
+  console.log(`server is running on port here ${PORT}`);
+});
 
-module.exports = app
+module.exports = app;
